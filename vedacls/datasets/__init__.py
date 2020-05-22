15f1df1851589221ccdfc8526c9562f4797bc3b5
chwang <chenhao.wang@media-smart.cn>
2020-05-22 19:41:56 +0800

    add requirements

--- vedacls/datasets/__init__.py
+++ vedacls/datasets/__init__.py
@@ -1,9 +1,8 @@
 from .loader import build_dataloader
 from .builder import build_dataset
 from .datasets import ImageFolder
-from .datasets_rgbd import ImageFolderRGBD
 from .registry import DATASETS, PIPELINES
 
 __all__ = ['build_dataloader', 'build_dataset',
-           'ImageFolder', 'ImageFolderRGBD',
+           'ImageFolder',
            'DATASETS', 'PIPELINES']
